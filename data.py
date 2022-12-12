e7d7e6dc63144ec1ca0f69915239fd8a93718a2c
Pratham Agrawal <prathamagrawal1205@gmail.com>
2022-12-12 12:20:34 +0530

    Removed data log files

--- data.py
+++ data.py
@@ -9,10 +9,3 @@ sales_train=pd.read_csv('./data/sales_train.csv')
 
 print("Unique Categories: "+str(len(categories['item_category_name'].unique())))
 categoriesd=categories.set_index('item_category_id').T.to_dict('list')
-
-
-
-item_category_names=[]
-for ri,rd in items.iterrows():
-    item_category_names.append(categoriesd[rd['item_category_id']])
-items['item_category_names']=item_category_names 
